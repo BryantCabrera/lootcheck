bb63dd6ab25cc8ae014bcbf27bc55ecfc46096c6
Bryant <cabrera.bryant@gmail.com>
2019-06-26 12:13:00 -0700

    Bryant creates test for Wallet's input render.

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -13,4 +13,9 @@ describe('Wallet', () => {
     it('displays the balance from props', () => {
         expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
     });
+
+    // even though we already make sure Wallet renders, we want to make sure we have an input to type into
+    it('creates an input to deposit into or withdraw from the balance', () => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
 });
