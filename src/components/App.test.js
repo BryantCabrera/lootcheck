9bc506ef7be0dcbbf54a3b8a4bcb606e5466d79d
Bryant <cabrera.bryant@gmail.com>
2019-06-25 21:00:25 -0700

    Bryant creates index.js and makes sure App.test.js passes all initial tests.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import { exportAllDeclaration } from '@babel/types';
 
 describe('App', () => {
     const app = shallow(<App />);
 
     it('renders properly', () => {
-        exportAllDeclaration(app).toMatchSnapshot();
+        expect(app).toMatchSnapshot();
     });
 });
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// render function handles rendering our react portion on the dom
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('root'));
