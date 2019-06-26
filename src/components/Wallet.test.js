c53d0559c1a7f19d1e199a90a06d1f95c6179544
Bryant <cabrera.bryant@gmail.com>
2019-06-26 12:18:19 -0700

    Bryant creates tests for updating Wallet state on input change.

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -18,4 +18,20 @@ describe('Wallet', () => {
     it('creates an input to deposit into or withdraw from the balance', () => {
         expect(wallet.find('.input-wallet').exists()).toBe(true);
     });
+
+    describe('when the user types into the wallet input', () => {
+        // when input on our DOM is rendered, it is a string by default and not converted automatically into a number
+        const userBalance = '25';
+
+        beforeEach(() => {
+            wallet.find('.input-wallet')
+                .simulate('change', { target: { value: userBalance } });
+        });
+
+        it('updates the local wallet balance in `state` and converts it to a number', () => {
+            // parseInt(numberToConver, baseOfNumber)
+                // use 10 to be in decimal system
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+    });
 });
