d406377736dc645fcafe390daaabca8bdea02aa4
Bryant <cabrera.bryant@gmail.com>
2019-06-26 14:01:05 -0700

    Bryant creates test for reading balance from cookies.

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,15 +1,24 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-    it('sets a balance', () => {
+    describe('when initializing', () => {
         const balance = 10;
-
-        // reducer takes 2 parameters: 
-            // previousState; in the beginning, it's undefined
-            // an object containing data; where actions are taken care of
-        expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-            .toEqual(balance);
+    
+        it('sets a balance', () => {
+            // reducer takes 2 parameters: 
+                // previousState; in the beginning, it's undefined
+                // an object containing data; where actions are taken care of
+            expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+                .toEqual(balance);
+        });
+    
+        describe('then re-initializing', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReducer2(undefined, {})).toEqual(balance);
+            });
+        });
     });
 
     it('deposits into the balance', () => {
