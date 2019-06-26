a94f922d43e736ee1a4925e49eee78456055c3ef
Bryant <cabrera.bryant@gmail.com>
2019-06-26 13:18:22 -0700

    Bryant creates mock withdraw function and tests if a simulated button click calls the function with correct parameters.

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -4,7 +4,8 @@ import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
     const mockDeposit = jest.fn();
-    const props = { balance: 20, deposit: mockDeposit }
+    const mockWithdraw = jest.fn();
+    const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw }
     const wallet = shallow(<Wallet {...props} />);
 
     it('renders properly', () => {
@@ -38,9 +39,18 @@ describe('Wallet', () => {
         describe('and the user wants to make a deposit', () => {
             beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
 
-            it('dispatches the `deposit()` it receives from props with local balance', () => {
+            it('dispatches the `deposit()` it receives from props with the local balance', () => {
                 // check mock fn was called and passed in the correct argument
                 expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+            });
+        });
+
+        describe('and the user wants to make a withdrawal', () => {
+            beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+
+            it('dispatches the `withdraw()` it receives from props with the local balance', () => {
+                // check mock fn was called and passed in the correct argument
+                expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
             });
         });
     });
