1bc307b5e4d621244d846a97e6799fe3fec35ef5
Bryant <cabrera.bryant@gmail.com>
2019-06-26 01:43:07 -0700

    Bryant creates test for displaying balance from props.

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -12,9 +12,11 @@ export class Wallet extends Component {
 }
 
 // connect() returns a function that we can use to connect Wallet
-// ii.Connect takes 2 parameters
+// Connect takes 2 parameters
     // (a)	Function which describes what part of the redux store we want to use on this component; a.k.a. MapStateToProps; that data will now be available in props
     // (b)	Describes what action creators we want to use on the component to send data to the redux store
         // (i)	Can be null if not using it
 
+// We need to export the component itself and import as { } since it’s not the default export
+    // This is because the connected component export has a lot of context expectations that relate to that
 export default connect(state => { balance: state}, null)(Wallet);
