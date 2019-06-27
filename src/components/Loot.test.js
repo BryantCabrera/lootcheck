0ac1e8f589d80da09e2d47173c5fe1242842d736
Bryant <cabrera.bryant@gmail.com>
2019-06-26 23:40:59 -0700

    Bryant creates test for calling fetchBitcoin on componentDidMount.

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { Loot } from './Loot';
 
 describe('Loot', () => {
-    const loot = shallow(<Loot />);
+    const mockFetchbitcoin = jest.fn();
+    const props = { balance: 10, bitcoin: {} }
+    let loot = shallow(<Loot {...props} />);
 
     it('renders properly', () => {
         expect(loot).toMatchSnapshot();
+    });
+
+    describe('when mounted', () => {
+        beforeEach(() => {
+            props.fetchBitcoin = mockFetchbitcoin;
+
+            // We need mount function from enzyme instead of shallow
+                // This renders all inner children and fires all life cycle hooks of a react component(like componentDidMount)
+            loot = mount(<Loot {...props} />);
+        });
+
+        it('dispatches the `fetchBitcoin()` method it receives from props', () => {
+            expect(mockFetchbitcoin).toHaveBeenCalled();
+        });
     });
 });
