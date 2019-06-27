c8d2ffd7d821eae224f67ad25113dab5629cecf5
Bryant <cabrera.bryant@gmail.com>
2019-06-26 22:45:01 -0700

    Bryant imports rootReducer.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // render function handles rendering our react portion on the dom
 import { render } from 'react-dom';
 import { createStore } from 'redux';
+import rootReducer from './reducers';
 import rootReducer from './reducers/balance';
 import { Provider } from 'react-redux'; // provides redux store to whole application
 import App from './components/App';
