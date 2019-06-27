6edce2c4bd10abadaaa2ea1f676a0758bdb01d43
Bryant <cabrera.bryant@gmail.com>
2019-06-26 23:57:55 -0700

    Bryant creates computerBitcoin().

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -8,9 +8,20 @@ export class Loot extends Component {
         this.props.fetchBitcoin();
     }
 
+    computeBitcoin() {
+        const { bitcoin } = this.props;
+
+        // guard clause = in the middle of our test, if we execute some behavior and run a condition and check that it's true, then we immediately return out of the function
+        if (Object.keys(bitcoin).length === 0) return '';
+
+        // use .replace(‘,’, ‘’) to replace all commas with nothing
+            // replace(‘itemToReplace’, ‘whatToReplaceItWith’)
+        return this.props.balance / parseInt(bitcoin.bpi.USD.rate.replace(',', ''), 10);
+    }
+
     render() {
         return (
-            <h3>Bitcoin balance:</h3>
+            <h3>Bitcoin balance: {this.computeBitcoin()}</h3>
         )
     }
 }
