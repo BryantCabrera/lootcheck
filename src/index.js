d6d507005d7e1222c7c66302b67b18de0a8cbbcb
Bryant <cabrera.bryant@gmail.com>
2019-06-26 22:50:55 -0700

    Bryant applies redux middleware thunk to our application at the root.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 // render function handles rendering our react portion on the dom
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import rootReducer from './reducers/balance';
 import { Provider } from 'react-redux'; // provides redux store to whole application
 import App from './components/App';
 
-const store = createStore(rootReducer);
+// apply redux middleware thunk to our application at the root
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
     <Provider store={store}>
